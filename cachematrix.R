655271a6e15eaab423e26010bde8b0e685995ce3
infinitesupermagic <infinitesupermagic@gmail.com>
2015-04-25 16:32:33 -0700

    Added cachematrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together to create a cache of a matrix.
+## One function is a list of functions that manipulate the matrix
+## The other function grabs the cache or creates a cache 
 
-## Write a short comment describing this function
+## This function is a list of functions that receives a matrix as in an put
+## It can perform a set, get, setsolve and getsolve function 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function takes a makeCacheMatrix function as input
+## checks and grabs the cached Matrix, otherwise, creates a cached Matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
